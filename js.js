7ffaeae7d2c70f03fb6cf92a746f592906ea4617
Catherine Siller <cathysiller503@gmail.com>
2015-08-21 21:35:24 -0500

    completely changed new personal site
    html, css, js, jquery

--- js.js
+++ js.js
@@ -1,11 +1,5 @@
 // Parallax
-if('ontouchstart' in window == false){
-  $('body').mousemove(function(e){
-    var moveX = (e.pageX * -1 / 25);
-    var moveY = (e.pageY * -1 / 25);
-    $('.outer').css('background-position', moveX + 'px ' + moveY + 'px');
-  });
-}
+
 
 // Set background Image
 $('ul#images li').click(function(){
